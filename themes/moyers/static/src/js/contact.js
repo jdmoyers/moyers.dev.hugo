6ff47baf15e90e2aff5d2a83d7895a6f2f616d5b
Joshua Moyers <josh@moyers.dev>
2019-09-07 13:16:04 -0400

    Form validation

--- themes/moyers/static/src/js/contact.js
+++ themes/moyers/static/src/js/contact.js
@@ -1,22 +1,80 @@
 var form = document.getElementById('contact-form');
+var validateFields = document.querySelectorAll('[data-val]');
 
-if(typeof(form) != null && form != null) {
+validateFields.forEach((field) => {
+    field.addEventListener('blur', function() {
+        validateField(this);
+    })
+});
+
+if(form != null) {
     form.addEventListener('submit', function(e) {
         e.preventDefault();
-        var name = document.getElementById('name').value;
-        var email = document.getElementById('email').value;
-        var message = document.getElementById('message').value;
+        
+        var fields = form.querySelectorAll('[data-val]');
 
-        if(!name) {
-            console.log('Missing name');
-        }
+        fields.forEach((field) => {
+            validateField(field);
+        });
 
-        if(!email) {
-            console.log('Missing email');
-        }
-
-        if(!message) {
-            console.log('Missing message');
+        var errors = form.querySelectorAll('label.error').length;
+        console.log(errors);
+        if(errors > 0) {
+            console.log('throw errors');
+        } else {
+            console.log('submit');
         }
     });
+}
+
+
+function validateField(field) {
+    switch (field.type.toLowerCase()) {
+        case 'text':
+        case 'textarea':
+            if(!field.value) {
+                toggleValidation(field, 'invalid');
+            } else {
+                toggleValidation(field, 'valid');
+            }
+            break;
+        
+        case 'email':
+            if(validateEmail(field.value)) {
+                toggleValidation(field, 'valid');
+            } else {
+                toggleValidation(field, 'invalid');
+            }
+            break;
+    }
+}
+
+function validateEmail (email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+function toggleValidation(field, state) {
+    var fieldName = field.name;
+    var label = form.querySelector(`[for=${fieldName}]`);
+    var errorElement = field.parentNode.querySelector('.form-error');
+    var errorMessage = field.getAttribute('data-val');
+
+    if(state == 'invalid') {
+        field.classList.add('error');
+        label.classList.add('error');
+        errorElement.innerHTML = errorMessage;
+        errorElement.classList.add('show');
+    } else {
+        if(field.classList.contains('error')) {
+            field.classList.remove('error');
+        }
+
+        if(label.classList.contains('error')) {
+            label.classList.remove('error');
+        }
+
+        if(errorElement.classList.contains('show')) {
+            errorElement.classList.remove('show');
+        }
+    }
 }
